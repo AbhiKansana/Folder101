778dfa44a531246bf5e99a27eb959c905a0bbd22
AbhiKansana <abhikansana05@gmail.com>
2022-03-15 01:12:38 +0530

    added few more file

---
+++ js/cart.js
@@ -0,0 +1,40 @@
+let arrCart = JSON.parse(localStorage.getItem("Cart11"))
+let price= 0
+
+function display(arr){
+arr.map(function(ele){
+    let som = `
+    
+    <div class="menbox">
+    <img  src="${ele.imageUrl}">
+    <p class="name">${ele.shirtNmae}</p>
+    <p class="price">&#8377;${ele.price}</p>
+    <p class="member">${ele.member}</p>
+    <button onclick="removeCart(${JSON.stringify(ele).split('"').join("&quot;")})">Remove</button>
+    </div>
+    
+    
+    `
+    price +=  +ele.price
+    document.getElementById("main").innerHTML += som
+})
+  document.getElementById("price").innerHTML = "Total Price : " + price
+}
+display(arrCart)
+
+console.log(price)
+
+
+function removeCart(ele){
+    let arr00 = JSON.parse(localStorage.getItem("Cart11"))
+    console.log(arr00)
+   let arr99 =  arr00.filter(function(i){
+        return i.shirtNmae !== ele.shirtNmae
+    })
+    console.log(arr99)
+    localStorage.setItem("Cart11",JSON.stringify(arr99))
+    document.getElementById("main").innerHTML = ""
+    price = 0
+    display(arr99)
+    
+}
---
+++ js/desc.js
@@ -0,0 +1,22 @@
+
+let arr77 = JSON.parse(localStorage.getItem("itemNo1"))
+
+
+
+let som =`
+        <img src="${arr77[0].imageUrl}">
+        <div>
+        <p class="name">${arr77[0].shirtNmae}</p>
+        <p class="price">&#8377;${arr77[0].price}</p>
+        <p class="member">${arr77[0].member}</p>
+        <button onclick="toCart(${JSON.stringify(arr77[0]).split('"').join("&quot;")})">Add to Cart</button>
+        </div>
+
+`
+document.getElementById("main").innerHTML = som
+
+let arrCart11 = JSON.parse(localStorage.getItem("Cart11")) || []
+function toCart(ele){
+    arrCart11.push(ele)
+  localStorage.setItem("Cart11",JSON.stringify(arrCart11))
+}
